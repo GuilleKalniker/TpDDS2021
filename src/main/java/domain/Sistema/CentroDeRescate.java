e789a94cdea19378ffca50ca0c3f821bf117bdb9
NicoMore <nicomorelli47@gmail.com>
2021-05-05 18:43:02 -0300

    EsteticaV2

--- src/main/java/domain/Sistema/CentroDeRescate.java
+++ src/main/java/domain/Sistema/CentroDeRescate.java
@@ -30,9 +30,9 @@ public class CentroDeRescate {
   }
 
   /*
-  obtenerListaDeQRs()
-  Obtiene todos los QRs de las mascotas ya registradas.
-   */
+  * obtenerListaDeQRs()
+  * Obtiene todos los QRs de las mascotas ya registradas.
+  */
   public List<Integer> obtenerListaDeQRs() {
     return this.mascotasRegistradas.stream().map(unaMascota -> unaMascota.getQr()).collect(Collectors.toList());
   }
@@ -42,9 +42,9 @@ public class CentroDeRescate {
   }
 
   /*
-    otorgarQR()
-    Otorga un nuevo QR aleatorio no repetido.
-    */
+  * otorgarQR()
+  * Otorga un nuevo QR aleatorio no repetido.
+  */
   public Integer otorgarQR() {
     while(true) { // Ciclo infinito hasta obtener un QR valido
       Integer QR = new Random().nextInt(10000); // QRs hasta 10000
@@ -55,9 +55,9 @@ public class CentroDeRescate {
   }
 
   /*
-  notificarMascotaEncontrada(1)
-  Obtiene una mascota a partir del QR registrado en el estado y luego notifica al duenio.
-   */
+  * notificarMascotaEncontrada(1)
+  * Obtiene una mascota a partir del QR registrado en el estado y luego notifica al duenio.
+  */
   public void notificarMascotaEncontrada(EstadoMascotaPerdida unEstado) { // No deberia ser inmediato (no entiendo por que no), por ahora lo es
 
     MascotaRegistrada mascota = this.identificarMascota(unEstado.getQrMascotaPerdida());
@@ -66,22 +66,26 @@ public class CentroDeRescate {
   }
 
   /*
-  Filtra la lista de estadoMascotaPerdida y le notifica a sus dueños que fueron encontradas
+  * notificarMascotasDeLosUltimos10Dias()
+  * Filtra la lista de estadoMascotaPerdida y le notifica a sus dueños que fueron encontradas
   */
  public void notificarMascotasDeLosUltimos10Dias(){
     this.listarMascotasPerdidasEnUltimosDiezDias().stream().forEach(m -> this.notificarMascotaEncontrada(m));
   }
 
   /*
-  identificarMascota(1)
-  Obtiene una mascota de la lista de mascotas registradas a partir de un QR.
-   */
+  * identificarMascota(1)
+  * Obtiene una mascota de la lista de mascotas registradas a partir de un QR.
+  */
   public MascotaRegistrada identificarMascota(Integer unQR) { // Podria llevar mas parametros del estado, pero con la key QR no hace falta
     return this.mascotasRegistradas.stream()
         .filter(unaMascota -> unaMascota.getQr().equals(unQR)).findFirst().get();
   }
 
-  // Devuelve la lista de las mascotas perdidas de los ultimos 10 dias
+  /*
+  * listarMascotasPerdidasEnUltimosDiezDias()
+  * Devuelve la lista de las mascotas perdidas de los ultimos 10 dias
+  */
   public List<EstadoMascotaPerdida> listarMascotasPerdidasEnUltimosDiezDias() {
 
     return this.estadosMascotasPerdidas.stream()
@@ -93,9 +97,9 @@ public class CentroDeRescate {
   }
 
   /*
-  listarMascotasEncontradasEnUltimosDiezDias()
-  Filtra lista de estados a partir de las fechas para luego mappear la mascota correspondiente al estado para generar la lista final. Dicha lista no es printeable.
-   */
+  * listarMascotasEncontradasEnUltimosDiezDias()
+  * Filtra lista de estados a partir de las fechas para luego mappear la mascota correspondiente al estado para generar la lista final. Dicha lista no es printeable.
+  */
   public List<MascotaRegistrada> listarMascotasEncontradasEnUltimosDiezDias() {
 
     return listarMascotasPerdidasEnUltimosDiezDias().stream()
@@ -103,19 +107,19 @@ public class CentroDeRescate {
   }
 
   /*
-  pasoEntreUltimosDiezDias(1)
-  Verifica si una fecha se encuentra dentro de los ultimos 10 dias.
-  compareTo devuelve negativo porque (unaFecha < now) (o deberia, primera validacion evita errores), entonces se fija si mayor a -10 para entrar en los ultimos 10 dias.
-   */
+  * pasoEntreUltimosDiezDias(1)
+  * Verifica si una fecha se encuentra dentro de los ultimos 10 dias.
+  * compareTo devuelve negativo porque (unaFecha < now) (o deberia, primera validacion evita errores), entonces se fija si mayor a -10 para entrar en los ultimos 10 dias.
+  */
   private boolean pasoEntreUltimosDiezDias(LocalDate unaFecha) {
     //return unaFecha.compareTo(LocalDate.now()) < 0 && unaFecha.compareTo(LocalDate.now()) > -10;
     return ChronoUnit.DAYS.between(unaFecha, LocalDate.now()) <= 10;
   }
 
   /*
-  validarQR(1)
-  Recibe un QR y verifica si pertenece al centro, de no pertenecer lanza una excepcion.
-   */
+  * validarQR(1)
+  * Recibe un QR y verifica si pertenece al centro, de no pertenecer lanza una excepcion.
+  */
   public void validarQR(Integer unQR) throws Exception {
     if (!this.obtenerListaDeQRs().contains(unQR)) {
       throw new Exception();
